a1586a4c3e1487d5b6da89450aa60e58507b7462
jessisaz <isa.zou.26@gmail.com>
2023-04-27 17:09:41 +0800

    minor changes

--- Includes/signup.inc.php
+++ Includes/signup.inc.php
@@ -1,2 +1,39 @@
 <?php
 
+if (isset($_POST["submit"])) {
+    $username = $_POST["uid"];
+    $email = $_POST["email"];
+    $pwd = $_POST["pwd"];
+
+
+        
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputSignup($email, $username, $pwd) !== false) {
+        header("location: ../index.php?error=emptyinput");
+        exit();
+    }
+    if (invalidUid($username) !== false) {
+        header("location: ../index.php?error=invaliduid");
+        exit();
+    }
+    if (invalidEmail($email) !== false) {
+        header("location: ../index.php?error=invalidemail");
+        exit();
+    }
+    if (uidExists($conn, $username, $email) !== false) {
+        header("location: ../index.php?error=usernametaken");
+        exit();
+    }
+
+    createUser($conn, $email, $username, $pwd);
+
+}
+else {
+    header("location: ../index.php");
+}
+
+
+
+?>
