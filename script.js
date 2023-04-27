610dc77d3256ee94f3a09e1fbe58a94fe0359b7a
ccsaltyfish <86415345+ccsaltyfish@users.noreply.github.com>
2023-04-27 16:32:28 +0800

    add the js file for the reservation, calendar

--- script.js
+++ script.js
@@ -20,3 +20,167 @@ iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
     wrapper.classList.remove('active');
 });
+
+/* reservation start */
+
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
+
+nextBtnFirst.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+nextBtnSec.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "-50%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+submitBtn.addEventListener("click", function(){
+    event.preventDefault();
+    slidePage.style.marginLeft = "-75%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+  
+prevBtnSec.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "0%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+});
+prevBtnThird.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+});
+
+/* reservation end */
+
+/* reservation: calendar start */
+
+const date = new Date();
+const currentYear = new Date().getFullYear();
+const currentMonth = new Date().getMonth();
+const today = new Date().getDate();
+
+var slecteddate;
+
+const renderCalendar = (x) => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.querySelector(".month h1").innerHTML = months[date.getMonth()];
+  document.querySelector(".month h3").innerHTML = date.getFullYear();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+      if (i < today && date.getMonth() == currentMonth && date.getFullYear() == currentYear) {
+        days += `<div class="prev-date">${i}</div>`
+      } else if (date < new Date(currentYear, currentMonth-1, today)) {
+        days += `<div class="prev-date">${i}</div>`
+      } else if (i == x) {
+        days += `<div class="today" onclick=clickdate(this)>${i}</div>`;
+      } else {
+        days += `<div onclick=clickdate(this)>${i}</div>`;
+      }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+
+document.querySelector(".prev_month").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next_month").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+
+function clickdate(event) {
+  
+  if (slecteddate === `${event.innerHTML}/${date.getMonth() + 1}`) {
+    event.style.backgroundColor = "#FFFFFF"
+    return;
+  }
+  slecteddate = `${event.innerHTML}/${date.getMonth() + 1}`;
+  console.log(slecteddate);
+  
+  renderCalendar(event.innerHTML)
+}
+
+renderCalendar();
+
+/* reservation: calendar end */
